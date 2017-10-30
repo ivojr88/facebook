b4056043e5ec26348dd0a4a305b6832bd1229d38
ckevinhill <hill.ck@pg.com>
2017-10-30 16:03:39 -0400

    Implementation of User Specific access_token retrieval

--- Facebook Nodes/src/com/pg/facebook/api/connector/node/FacebookConnectorNodeDialog.java
+++ Facebook Nodes/src/com/pg/facebook/api/connector/node/FacebookConnectorNodeDialog.java
@@ -1,12 +1,22 @@
 package com.pg.facebook.api.connector.node;
 
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URI;
+import java.nio.charset.Charset;
 
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JTextArea;
 
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 import org.knime.core.data.DataTableSpec;
 import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.NodeSettingsRO;
@@ -33,7 +43,9 @@ public class FacebookConnectorNodeDialog extends StandardTrackedNodeDialogPane {
 	
     private JTextArea txtRefreshToken = new JTextArea();
     private DefaultComboBoxModel<String> cbmApiSelection = new DefaultComboBoxModel<String>();
+    private JButton btnCreateToken = new JButton("Get User Token");
 	
+    
     /**
      * New pane for configuring FacebookConnector node dialog.
      * This is just a suggestion to demonstrate possible default dialog
@@ -46,6 +58,9 @@ public class FacebookConnectorNodeDialog extends StandardTrackedNodeDialogPane {
     	JComboBox<String> cbxApiSelection = new JComboBox<String>(cbmApiSelection);
     	cbxApiSelection.addActionListener(new TokenChangeAction());
     	
+    	btnCreateToken.addActionListener(new ShowBrowserAction());
+    	btnCreateToken.setVisible(false);
+    	
     	for (String type: configuration.getAuthTypes()) {
     		cbmApiSelection.addElement(type);
     	}
@@ -56,6 +71,7 @@ public class FacebookConnectorNodeDialog extends StandardTrackedNodeDialogPane {
     			new PanelBuilder()
     				.add("Credentials", cbxApiSelection )
     				.add("User Token", txtRefreshToken )
+    				.add("", btnCreateToken )
     				.build()
     		)
     	);
@@ -81,6 +97,9 @@ public class FacebookConnectorNodeDialog extends StandardTrackedNodeDialogPane {
 		
 		// Initialize
 		String selectedItem = (String)cbmApiSelection.getSelectedItem();
+		btnCreateToken.setVisible("User Specific".equals(selectedItem));
+		
+		
 		if ( selectedItem != null && ( configuration.getAccessToken() == null || configuration.getAccessToken().isEmpty() ) ) {
 			configuration.setAccessToken(configuration.getTokenFromAuthType(selectedItem) );
 		}
@@ -97,12 +116,83 @@ public class FacebookConnectorNodeDialog extends StandardTrackedNodeDialogPane {
 			String selectedItem = (String)cbmApiSelection.getSelectedItem();
 			if ( selectedItem == null ) return;
 			
+			/*
+			 * Get configuration value:
+			 */
 			String token = configuration.getTokenFromAuthType(selectedItem);
 			configuration.setAccessToken(token);
 			txtRefreshToken.setText(configuration.getAccessToken());
 			
+			/*
+			 * If User Specified show optional button
+			 */
+			btnCreateToken.setVisible("User Specific".equals(selectedItem));
 		}
     	
+    }
+    
+    class ShowBrowserAction implements ActionListener {
+    	
+    	@Override
+    	public void actionPerformed(ActionEvent e) {
+    		
+    		// Create new thread for SWT Browser instance:
+    		Thread t = new Thread(new Runnable() {
+				
+				@Override
+				public void run() {
+				
+					final Display display = new Display();
+		    		final Shell shell = new Shell ( display );
+		    		final Browser browser = new Browser ( shell, SWT.NONE);
+					
+		    		// Setup Shell:
+		    		display.asyncExec(new Runnable() {
+			    		public void run() {
+			    			
+			    			shell.setSize(800, 600);
+			    			browser.setLayoutData(new GridData(GridData.FILL_BOTH));
+			    			browser.setSize(800, 600);
+			    			browser.setUrl("https://www.facebook.com/v2.10/dialog/oauth?client_id=1600065610276063&response_type=token&scope=ads_management&redirect_uri=https://www.pg.com/connect/login_success.html");
+			    			shell.open();
+			    		}
+		    		});
+					
+		    		// Check to see if has arrived at confirmation URL:
+	    			while (!shell.isDisposed()) {
+		    			
+						display.asyncExec(new Runnable() {
+							@Override
+							public void run() {
+								try {
+									if ( browser != null && !browser.isDisposed() && browser.getUrl().startsWith("https://www.pg.com/connect/login_success.html") ) {
+										String token = URLEncodedUtils.parse( new URI(browser.getUrl()).getFragment(), Charset.forName("UTF-8") )
+															.stream()
+															.filter( pair -> "access_token".equals(pair.getName()))
+															.map(pair -> pair.getValue())
+															.findFirst()
+															.orElse("");
+															
+										txtRefreshToken.setText( token );
+										shell.close();
+										display.dispose();
+									}
+								} catch ( Exception exc ) {
+									System.out.println("Error...");
+								}
+							}
+						});
+		    			
+		    			if ( !display.readAndDispatch()  )
+							display.sleep();
+					}					
+					
+				}
+			});
+    		
+    		t.start();
+    		    			
+    	}	
     }
 	
 }
